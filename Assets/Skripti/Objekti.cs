4fbadcfdc1de7ab37bbf0ea41a18a6177a2e67ed
Volters <kevinsvolters12@gmail.com>
2023-06-06 08:32:08 +0300

    pievinoju masina objektus

--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Objekti : MonoBehaviour {
-	public GameObject atkritumuMasina;
-	public GameObject atraPalidziba;
-	public GameObject autobuss;
+	//pievienoju masinas objektus
+	public GameObject atkritumuMasina, atraPalidziba, autobuss, b2, cementaMasina, e46, e61, eskavators, policija, traktors1, traktors5, ugunsdzeseji;
+
 
 	[HideInInspector]
 	public Vector2 atkrMKoord;
@@ -13,6 +13,24 @@ public class Objekti : MonoBehaviour {
 	public Vector2 atrPKoord;
 	[HideInInspector]
 	public Vector2 bussKoord;
+	[HideInInspector]
+	public Vector2 b2Koord;
+	[HideInInspector]
+	public Vector2 cementaKoord;
+	[HideInInspector]
+	public Vector2 e46Koord;
+	[HideInInspector]
+	public Vector2 e61Koord;
+	[HideInInspector]
+	public Vector2 eskavatorsKoord;
+	[HideInInspector]
+	public Vector2 policijaKoord;
+	[HideInInspector]
+	public Vector2 traktors1Koord;
+	[HideInInspector]
+	public Vector2 traktors5Koord;
+	[HideInInspector]
+	public Vector2 ugunsdzesejiKoord;
 
 	public Canvas kanva;
 
@@ -24,7 +42,7 @@ public class Objekti : MonoBehaviour {
 
 	public GameObject pedejaisVilktais = null;
 
-
+	//iegust masinu atrasanas vietu
 	void Start() {
 		atkrMKoord =
         atkritumuMasina.GetComponent<RectTransform>().localPosition;
@@ -34,5 +52,32 @@ public class Objekti : MonoBehaviour {
 
         bussKoord =
 		autobuss.GetComponent<RectTransform>().localPosition;
+
+		b2Koord =
+			b2.GetComponent<RectTransform>().localPosition;
+
+		cementaKoord =
+			cementaMasina.GetComponent<RectTransform>().localPosition;
+
+		e46Koord =
+			e46.GetComponent<RectTransform>().localPosition;
+
+		e61Koord =
+			e61.GetComponent<RectTransform>().localPosition;
+
+		eskavatorsKoord =
+			eskavators.GetComponent<RectTransform>().localPosition;
+
+		policijaKoord =
+			policija.GetComponent<RectTransform>().localPosition;
+
+		traktors1Koord =
+			traktors1.GetComponent<RectTransform>().localPosition;
+
+		traktors5Koord =
+			traktors5.GetComponent<RectTransform>().localPosition;
+
+		ugunsdzesejiKoord =
+			ugunsdzeseji.GetComponent<RectTransform>().localPosition;
     }
 }
